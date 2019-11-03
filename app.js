3b055c9117915d7a58fa7a0023c59343b0a227b7
Michael <Michael.SS.Dev@gmail.com>
2019-11-03 21:21:37 +0900

    fourth commit

--- app.js
+++ app.js
@@ -6,19 +6,9 @@ import bodyParser from "body-parser";
 
 const app = express();
 
-const PORT = 4000;
-
-const handleListening = () =>
-  console.log(`Listening on: http://localhost:${PORT}`);
-
 const handleHome = (req, res) => res.send("Hello from Home");
 
 const handleProfile = (req, res) => res.send("you are in profile");
-
-const betweenHome = (req, res, next) => {
-  console.log("I`m between");
-  next();
-};
 
 app.use(cookieParser());
 app.use(bodyParser.json());
@@ -26,10 +16,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(logger("dev"));
 
-app.use(betweenHome);
-
 app.get("/", handleHome);
 
 app.get("/profile", handleProfile);
 
-app.listen(PORT, handleListening);
+export default app;
